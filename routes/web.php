d88602fd0f96c7922004befa6f95fdc2d2532a81
Corax73 <ramaz999@inbox.ru>
2023-05-25 23:08:59 +0300

    added maincontroller, routes and layouts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(MainController::class)
+-> group(function () {
+    Route::get('/', 'index') -> name('main');
+    Route::get('/book-list', 'bookList') -> name('book-list');
+    Route::get('/singin', 'singin') -> name('singin');
 });
