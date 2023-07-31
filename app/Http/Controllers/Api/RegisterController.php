b6ec5382994d4a23f1746fac45edf9b0ba5959ee
Corax73 <ramaz999@inbox.ru>
2023-07-31 22:47:08 +0300

    added passport library, API routes, controllers and methods for registering and viewing books (test)

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    /**
+     * Api user registration
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validData = Validator::make($request->all(), [
+            'name' => 'required|unique:users',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if($validData->fails()) {
+            $response = [
+                'success' => false,
+                'message' => 'Validation Error.',
+                'data' => $validData->errors()
+            ];
+            return response()->json($response, 404);       
+        }
+        
+        $inputData = $request->all();
+        $inputData['password'] = bcrypt($inputData['password']);
+
+        $user = User::create($inputData);
+
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $success['name'] =  $user->name;
+
+        $response = [
+            'success' => true,
+            'data'    => $success,
+            'message' => 'User register successfully.',
+        ];
+        return response()->json($response, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(RegisterController::class)
+-> group(function () {
+    Route::post('/register', 'register');
+});
+
+Route::controller(BookController::class)
+-> group(function () {
+    Route::middleware('auth:api')->get('books/getAll', 'index');
 });
