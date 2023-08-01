b0ab41ed0a289c1e10a7111a35488fdbdd4c1d87
Corax73 <ramaz999@inbox.ru>
2023-08-01 10:22:30 +0300

    work with json from request

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Book;
+use Validator;
 
 class BookController extends Controller
 {
@@ -52,7 +53,32 @@ class BookController extends Controller
     {
         $input = json_encode($request->getContent(), JSON_UNESCAPED_UNICODE);
         $json = json_decode($input, true);
-        return response($json);
+        $a = trim($json, '{');
+        $a = trim($a, '}');
+        $b = preg_split("/[,]/", $a);
+        $result = [];
+        $result1 = [];
+        foreach ($b as $value) {
+            $result[] = explode(':', $value);
+        }
+        foreach ($result as $value) {
+            $str = trim(str_replace('"', '', $value[0]));
+            $result1[$str] = trim($value[1]);
+        }
+        $validData = Validator::make($result1, [
+            'title' => 'required|unique:books|min:3',
+            'slug' => 'required|unique:books',
+            'author' => 'required|min:5',
+            'description' => 'required|min:30',
+            'cover' => 'required'
+        ]);
+        if(!$validData->fails()){
+            $book = Book::find((integer)$result1['id']);
+            unset($result1['id']);
+            $book->update($result1);
+            return response('work');
+        }
+        return response('not work');
     }
 
     /**
